5f6f869a599b91f1601c68b77af282a1b5df6fa9
FelipeTeixeira3 <2022498@student.cct.ie>
2023-05-30 22:37:06 +0000

    Included favorite page

--- camera_app/src/app/services/photo.service.ts
+++ camera_app/src/app/services/photo.service.ts
@@ -1,32 +1,21 @@
 import { Injectable } from '@angular/core';
-import {Camera, CameraResultType, CameraSource, Photo} from "@capacitor/camera";
+import { Camera, CameraResultType, CameraSource, Photo } from "@capacitor/camera";
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { AlertController, Platform } from '@ionic/angular';
-
-
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class PhotoService {
   public photos: UserPhoto[] = [];
-  private photo_storage = 'photos';
-  private platform: Platform;
 
-  constructor(platform: Platform, private alertController: AlertController) { 
-    this.platform = platform;
-  }
+  constructor(private platform: Platform, private alertController: AlertController) { }
 
-
-
-  private async convertBase64(photo: Photo) {
+  private async convertBase64(photo: Photo): Promise<string | null> {
     if (photo.webPath) {
-      const res = await fetch(photo.webPath);
-      const blob = await res.blob();
-      
+      const blob = await (await fetch(photo.webPath)).blob();
       return await this.convertBlobTo64(blob) as string;
-    } 
+    }
     return null;
   }
 
@@ -34,80 +23,65 @@ export class PhotoService {
     return new Promise((resolve, reject) => {
       const reader = new FileReader();
       reader.onerror = reject;
-      reader.onload = () => {
-        resolve(reader.result);
-      }
+      reader.onload = () => resolve(reader.result);
       reader.readAsDataURL(blob);
-    })
+    });
   }
 
   private async saveToDevice(photo: Photo) {
     const base64data = await this.convertBase64(photo);
-    if (base64data) {
-  
-      const filename = `Picture taken at ${new Date().getTime()}.jpeg`;
-      const savedFile = await Filesystem.writeFile({
-        path: filename,
-        data: base64data,
-        directory: Directory.Data
-      })
-      console.log(filename);
-      console.log(savedFile);
-  
-      return {
-        filepath: filename,
-        webviewPath: photo.webPath
-      }
-    }
-  
-    // If base64data is null, return null or some default value
-    return null;
+    if (!base64data) return null;
+
+    const filename = `Picture taken at ${new Date().getTime()}.jpeg`;
+    const savedFile = await Filesystem.writeFile({
+      path: filename,
+      data: base64data,
+      directory: Directory.Data
+    });
+
+    console.log(filename);
+    console.log(savedFile);
+
+    return { filepath: filename, webviewPath: photo.webPath };
   }
-  
 
   async takePhoto() {
     const photo = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
-    })
+    });
   
     const alert = await this.alertController.create({
       header: 'Enter a subtitle',
       inputs: [
-        {
-          name: 'title',
-          type: 'text',
-          placeholder: 'Title'
-        }
+        { name: 'title', type: 'text', placeholder: 'Title' },
+        { name: 'favorite', type: 'checkbox', label: 'Favorite' }
       ],
       buttons: [
+        { text: 'Cancel', role: 'cancel' },
         {
-          text: 'Cancel',
-          role: 'cancel'
-        }, {
           text: 'Ok',
-          handler: data => {
-            const title = data.title;
-            
-            this.photos.unshift({
-              filepath: 'tbd',
-              webviewPath: photo.webPath,
-              title: title
-            })
-            console.log(this.photos.length);
-            this.saveToDevice(photo);
+          handler: async data => {
+            const savedPhoto = await this.saveToDevice(photo);
+            if (savedPhoto) {
+              this.photos.unshift({ filepath: savedPhoto.filepath, webviewPath: savedPhoto.webviewPath, title: data.title, favorite: data.favorite });
+              console.log(this.photos.length);
+            }
           }
         }
       ]
-    })
+    });
   
     await alert.present();
   }
+  
 }
 
 export interface UserPhoto {
   filepath: string;
   webviewPath?: string;
   title?: string;
+  favorite?: boolean;
 }
+
